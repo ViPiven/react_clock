37ea5bd1703a59881f31866254a7e39879201395
ViPiven <vitalijjpiven777@gmail.com>
2021-05-20 18:13:33 +0300

    Updates

---
+++ precache-manifest.f4ed25b55026809faac5af510ff5483c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "399f98e1bbad5b17e844700fb3680bc1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5ea8d3e8d10081d4953e",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ea8d3e8d10081d4953e",
+    "url": "./static/js/main.9d97d21a.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
